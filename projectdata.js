8680d29769106aba03ccd24978e06c6bb998691b
Luke <luke@scene.mov>
2025-01-15 01:00:16 -0500

    Added project modal functionality

---
+++ projectdata.js
@@ -0,0 +1,8 @@
+const data = {
+	max: [
+		{ type: 'h1', content: 'Max Promo' },
+		{ type: 'p', content: 'Working with the streaming platform, Max (formerly HBO Max), and popular TikTok creator, Elliott Walker, Scene was hired as the sole VFX studio for a short-form promo. Through our work, viewers are transported to the worlds of Dune and Jurassic World.' },
+		{ type: 'video', src: 'main.mp4' },
+		{ type: 'img', src: 'image1.webp' },
+	],
+};
