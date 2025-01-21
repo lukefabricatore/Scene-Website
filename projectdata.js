2b4b5067705c7d42636a543657c5aaf55ed40062
juliandalat <juliandalat@gmail.com>
2025-01-21 00:15:57 -0500

    finish css for open modal, add in new project

--- projectdata.js
+++ projectdata.js
@@ -1,8 +1,29 @@
 const data = {
+	drifting: [
+		{ type: "h1", content: "Driting" },
+		{
+			type: "p",
+			content:
+				"We had the opportunity to bring our VFX magic and our love for sci-fi together in 'Drifting'. A mini-series directed by Elliot Walker, Driting required CG and VFX work ranging from video effects like teleportation, all the way to full 3D built and rendered scenes.",
+		},
+		{
+			type: "caption",
+			content: "Client: Elliot Walker",
+		},
+		{
+			type: "p",
+			content: "Episode 1",
+		},
+		{ type: "video", src: "Episode_1.mp4" },
+	],
 	max: [
-		{ type: 'h1', content: 'Max Promo' },
-		{ type: 'p', content: 'Working with the streaming platform, Max (formerly HBO Max), and popular TikTok creator, Elliott Walker, Scene was hired as the sole VFX studio for a short-form promo. Through our work, viewers are transported to the worlds of Dune and Jurassic World.' },
-		{ type: 'video', src: 'main.mp4' },
-		{ type: 'img', src: 'image1.webp' },
+		{ type: "h1", content: "Max Promo" },
+		{
+			type: "p",
+			content:
+				"Working with the streaming platform, Max (formerly HBO Max), and popular TikTok creator, Elliott Walker, Scene was hired as the sole VFX studio for a short-form promo. Through our work, viewers are transported to the worlds of Dune and Jurassic World.",
+		},
+		{ type: "video", src: "main.mp4" },
+		{ type: "img", src: "image1.webp" },
 	],
 };
