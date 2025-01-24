bfb277c202017b48e2f9559981a6398de15e4279
juliandalat <juliandalat@gmail.com>
2025-01-23 19:16:48 -0500

    Fixed bugs w/ Luke on Scene meeting

--- projectdata.js
+++ projectdata.js
@@ -1,55 +1,56 @@
 const data = {
-  lg: [
-    { type: "h1", content: "LG Wonderbox Showcase Challenge 2023" },
-    {
-      type: "p",
-      content:
-        "LG Wonderbox Showcase Challenge 2023 is a competition that invites creators to submit a 3D billboard for a chance to win a cash prize, and a trip to see it in person, displayed in Times Square. Little did we know when we started this project, that we would be one of 3 finalists that got onto the billboard, and a special apparenance on the Drew Barrymore Show.",
-    },
-    { type: "video", src: "LifePaths Reel Video.mp4" },
-    { type: "video", src: "LG_Render_ProRes_CC.mp4" },
-    { type: "img", src: "PXL_20231213_203338472.MP.webp" },
-    { type: "img", src: "IMG_6151.webp" },
-  ],
+	lg: [
+		{ type: "h1", content: "LG Wonderbox Showcase Challenge 2023" },
+		{
+			type: "p",
+			content:
+				"LG Wonderbox Showcase Challenge 2023 is a competition that invites creators to submit a 3D billboard for a chance to win a cash prize, and a trip to see it in person, displayed in Times Square. Little did we know when we started this project, that we would be one of 3 finalists that got onto the billboard, and a special apparenance on the Drew Barrymore Show.",
+		},
+		{ type: "video", src: "LifePaths Reel Video.mp4" },
+		{ type: "video", src: "LG_Render_ProRes_CC.mp4" },
+		{ type: "img", src: "PXL_20231213_203338472.MP.webp" },
+		{ type: "img", src: "IMG_6151.webp" },
+	],
 
-  drifting: [
-    { type: "h1", content: "Driting" },
-    {
-      type: "p",
-      content:
-        "We had the opportunity to bring our VFX magic and our love for sci-fi together in 'Drifting'. A mini-series directed by Elliot Walker, Driting required CG and VFX work ranging from video effects like teleportation, all the way to full 3D built and rendered scenes.",
-    },
-    {
-      type: "caption",
-      content: "Client: Elliot Walker",
-    },
-    {
-      type: "h3",
-      content: "Episode 1",
-    },
-    { type: "video", src: "Episode_1.mp4" },
-    {
-      type: "h3",
-      content: "Episode 2",
-    },
-    { type: "video", src: "Episode_2.mp4" },
-    { type: "video", src: "EP2_Intro.mp4" },
-    { type: "video", src: "EP2_Final_Shot.mp4" },
-    {
-      type: "p",
-      content: "Assets",
-    },
-    { type: "img", src: "space_station.webp" },
-  ],
+	drifting: [
+		{ type: "h1", content: "Driting" },
+		{
+			type: "p",
+			content:
+				"We had the opportunity to bring our VFX magic and our love for sci-fi together in 'Drifting'. A mini-series directed by Elliot Walker, Driting required CG and VFX work ranging from video effects like teleportation, all the way to full 3D built and rendered scenes.",
+		},
+		{
+			type: "p",
+			desc: true,
+			content: "Client: Elliot Walker",
+		},
+		{
+			type: "h3",
+			content: "Episode 1",
+		},
+		{ type: "video", src: "Episode_1.mp4" },
+		{
+			type: "h3",
+			content: "Episode 2",
+		},
+		{ type: "video", src: "Episode_2.mp4" },
+		{ type: "video", src: "EP2_Intro.mp4" },
+		{ type: "video", src: "EP2_Final_Shot.mp4" },
+		{
+			type: "p",
+			content: "Assets",
+		},
+		{ type: "img", src: "space_station.webp" },
+	],
 
-  max: [
-    { type: "h1", content: "Max Promo" },
-    {
-      type: "p",
-      content:
-        "Working with the streaming platform, Max (formerly HBO Max), and popular TikTok creator, Elliott Walker, Scene was hired as the sole VFX studio for a short-form promo. Through our work, viewers are transported to the worlds of Dune and Jurassic World.",
-    },
-    { type: "video", src: "main.mp4" },
-    { type: "video", src: "Jurassic World Thumper.mov" },
-  ],
+	max: [
+		{ type: "h1", content: "Max Promo" },
+		{
+			type: "p",
+			content:
+				"Working with the streaming platform, Max (formerly HBO Max), and popular TikTok creator, Elliott Walker, Scene was hired as the sole VFX studio for a short-form promo. Through our work, viewers are transported to the worlds of Dune and Jurassic World.",
+		},
+		{ type: "video", src: "main.mp4" },
+		{ type: "video", src: "Jurassic World Thumper.mp4" },
+	],
 };
