74e75800df363bcf59e5b8f4324946898115d50d
juliandalat <juliandalat@gmail.com>
2025-02-07 00:20:29 -0500

    Add favicon, new brand video, and logo; update index and styles for improved layout

--- projectdata.js
+++ projectdata.js
@@ -1,109 +1,108 @@
 const data = {
-  lg: [
-    { type: "h1", content: "LG Wonderbox Showcase Challenge 2023" },
-    {
-      type: "p",
-      content:
-        "LG Wonderbox Showcase Challenge 2023 is a competition that invites creators to submit a 3D billboard for a chance to win a cash prize, and a trip to see it in person, displayed in Times Square. Little did we know when we started this project, that we would be one of 3 finalists that got onto the billboard, and a special apparenance on the Drew Barrymore Show.",
-    },
-    { type: "video", src: "LifePaths Reel Video.mp4" },
-    { type: "video", src: "LG_Render_ProRes_CC.mp4" },
-    { type: "img", src: "PXL_20231213_203338472.MP.webp" },
-    { type: "img", src: "IMG_6151.webp" },
-  ],
+	lg: [
+		{ type: "h1", content: "LG Wonderbox Showcase Challenge 2023" },
+		{
+			type: "p",
+			content:
+				"LG Wonderbox Showcase Challenge 2023 is a competition that invites creators to submit a 3D billboard for a chance to win a cash prize, and a trip to see it in person, displayed in Times Square. Little did we know when we started this project, that we would be one of 3 finalists that got onto the billboard, and a special apparenance on the Drew Barrymore Show.",
+		},
+		{ type: "video", src: "LifePaths Reel Video.mp4" },
+		{ type: "video", src: "LG_Render_ProRes_CC.mp4" },
+		{ type: "img", src: "PXL_20231213_203338472.MP.webp" },
+		{ type: "img", src: "IMG_6151.webp" },
+	],
 
-  drifting: [
-    { type: "h1", content: "Driting" },
-    {
-      type: "p",
-      content:
-        "We had the opportunity to bring our VFX magic and our love for sci-fi together in 'Drifting'. A mini-series directed by Elliot Walker, Driting required CG and VFX work ranging from video effects like teleportation, all the way to full 3D built and rendered scenes.",
-    },
-    {
-      type: "p",
-      desc: true,
-      content: "Client: Elliot Walker",
-    },
-    {
-      type: "h3",
-      content: "Episode 1",
-    },
-    { type: "video", src: "Episode_1.mp4" },
-    {
-      type: "h3",
-      content: "Episode 2",
-    },
-    { type: "video", src: "Episode_2.mp4" },
-    { type: "video", src: "EP2_Intro.mp4" },
-    { type: "video", src: "EP2_Final_Shot.mp4" },
-    {
-      type: "p",
-      content: "Assets",
-    },
-    { type: "img", src: "space_station.webp" },
-  ],
+	drifting: [
+		{ type: "h1", content: "Driting" },
+		{
+			type: "p",
+			content:
+				"We had the opportunity to bring our VFX magic and our love for sci-fi together in 'Drifting'. A mini-series directed by Elliot Walker, Driting required CG and VFX work ranging from video effects like teleportation, all the way to full 3D built and rendered scenes.",
+		},
+		{
+			type: "p",
+			desc: true,
+			content: "Client: Elliot Walker",
+		},
+		{
+			type: "h3",
+			content: "Episode 1",
+		},
+		{ type: "video", src: "Episode_1.mp4" },
+		{
+			type: "h3",
+			content: "Episode 2",
+		},
+		{ type: "video", src: "Episode_2.mp4" },
+		{ type: "video", src: "EP2_Intro.mp4" },
+		{ type: "video", src: "EP2_Final_Shot.mp4" },
+		{
+			type: "h3",
+			content: "Assets",
+		},
+		{ type: "img", src: "space_station.webp" },
+	],
 
-  max: [
-    { type: "h1", content: "Max Promo" },
-    {
-      type: "p",
-      content:
-        "Working with the streaming platform, Max (formerly HBO Max), and popular TikTok creator, Elliott Walker, Scene was hired as the sole VFX studio for a short-form promo. Through our work, viewers are transported to the worlds of Dune and Jurassic World.",
-    },
-    { type: "video", src: "main.mp4" },
-    { type: "video", src: "Jurassic World Thumper.mp4" },
-  ],
+	max: [
+		{ type: "h1", content: "Max Promo" },
+		{
+			type: "p",
+			content:
+				"Working with the streaming platform, Max (formerly HBO Max), and popular TikTok creator, Elliott Walker, Scene was hired as the sole VFX studio for a short-form promo. Through our work, viewers are transported to the worlds of Dune and Jurassic World.",
+		},
+		{ type: "video", src: "main.mp4" },
+		{ type: "videoauto", src: "JurassicWorldThumper.mp4" },
+	],
 
-  paxahauMetrotimes: [
-    { type: "h1", content: "Paxahau Metro Times Cover" },
-    {
-      type: "p",
-      content:
-        "Scene was approached by Paxahau to create a cover for Movement, an electronic music festival in the heart of Detroit city. The cover was featured in Metro Times, a local Detroit publication.",
-    },
-    { type: "videoauto", src: "Movment Metro Times Final Compressed.mp4" },
-    { type: "img", src: "Mouvement MetroTimes Update 2.webp" },
-    { type: "img", src: "MetroTimesCoverMockup.webp" },
-  ],
+	paxahauMetrotimes: [
+		{ type: "h1", content: "Paxahau Metro Times Cover" },
+		{
+			type: "p",
+			content:
+				"Scene was approached by Paxahau to create a cover for Movement, an electronic music festival in the heart of Detroit city. The cover was featured in Metro Times, a local Detroit publication.",
+		},
+		{ type: "videoauto", src: "Movment Metro Times Final Compressed.mp4" },
+		{ type: "img", src: "Mouvement MetroTimes Update 2.webp" },
+		{ type: "img", src: "MetroTimesCoverMockup.webp" },
+	],
 
-  reimagineLifeFoundation: [
-    { type: "h1", content: "Reimagine Life Foundation" },
-    {
-      type: "p",
-      content:
-        "Scene had the opportunity to work with the Reimagine Life Foundation to create a new logo and brand identity. The foundation is a non-profit organization that helps people who have had life-altering disabilities.",
-    },
-    { type: "img", src: "ReimagineLife_BrandGuidelines4_1.webp" },
-    { type: "img", src: "ReimagineLife_BrandGuidelines_3.webp" },
-    { type: "img", src: "ReimagineLife_BrandGuidelines_2.webp" },
-  ],
+	reimagineLifeFoundation: [
+		{ type: "h1", content: "Reimagine Life Foundation" },
+		{
+			type: "p",
+			content:
+				"Scene had the opportunity to work with the Reimagine Life Foundation to create a new logo and brand identity. The foundation is a non-profit organization that helps people who have had life-altering disabilities.",
+		},
+		{ type: "img", src: "ReimagineLife_BrandGuidelines4_1.webp" },
+		{ type: "img", src: "ReimagineLife_BrandGuidelines_3.webp" },
+		{ type: "img", src: "ReimagineLife_BrandGuidelines_2.webp" },
+	],
 
-  nothing3DBrand: [
-    { type: "h1", content: "Nothing Rebrand" },
-    {
-      type: "p",
-      content:
-        "Scene member Luke created a Brand Story video for Nothing, a tech company rooted in going against the grain. Hypothetical work.",
-    },
-    { type: "video", src: "brandvideo.mp4" },
-    { type: "img", src: "nothing2.webp" },
-    { type: "img", src: "nothing3.webp" },
-    { type: "img", src: "nothing9.webp" },
-    { type: "videoauto", src: "Nothing 12.mp4" },
-  ],
+	nothing3DBrand: [
+		{ type: "h1", content: "Nothing Rebrand" },
+		{
+			type: "p",
+			content: "Scene member Luke created a Brand Story video for Nothing, a tech company rooted in going against the grain. Hypothetical work.",
+		},
+		{ type: "video", src: "brandvideo.mp4" },
+		{ type: "img", src: "nothing2.webp" },
+		{ type: "img", src: "nothing3.webp" },
+		{ type: "img", src: "nothing9.webp" },
+		{ type: "videoauto", src: "Nothing 12.mp4" },
+	],
 
-  mechKeeb: [
-    { type: "h1", content: "Mechanical Keyboard Story Brand" },
-    {
-      type: "p",
-      content:
-        "Scene member Julian created a 3D brand story for a the mechanical keyboard online community. The video highlights the community's passion for mechanical keyboards and the culture that surrounds it.",
-    },
-    { type: "video", src: "keeb_final.mp4" },
-    { type: "img", src: "6.webp" },
-    { type: "img", src: "7.webp" },
-    { type: "img", src: "8.webp" },
-    { type: "img", src: "9.webp" },
-    { type: "img", src: "11.webp" },
-  ],
+	mechKeeb: [
+		{ type: "h1", content: "Mechanical Keyboard Story Brand" },
+		{
+			type: "p",
+			content:
+				"Scene member Julian created a 3D brand story for a the mechanical keyboard online community. The video highlights the community's passion for mechanical keyboards and the culture that surrounds it.",
+		},
+		{ type: "video", src: "keeb_final.mp4" },
+		{ type: "img", src: "6.webp" },
+		{ type: "img", src: "7.webp" },
+		{ type: "img", src: "8.webp" },
+		{ type: "img", src: "9.webp" },
+		{ type: "img", src: "11.webp" },
+	],
 };
